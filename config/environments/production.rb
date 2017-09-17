97f08839016bbe3c0d0e21dc78583a8c57dd0f8e
Nick Bolt <nick.bolt@redemptionmedia.co.uk>
2017-09-17 15:07:04 +0100

    :bug: Paperclip aws production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,14 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
 
   config.action_controller.forgery_protection_origin_check = false
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 end
